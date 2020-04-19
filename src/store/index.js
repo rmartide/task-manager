14e187deec0bf73f8f84d52625f49012722c6bb9
Rodrigo Martín de Lamo <mcgod69esp@gmail.com>
2020-04-19 22:56:21 +0200

    Added tasks to the store state and the logic to load all tasks.

--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,24 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import api from '@/services/api'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    tasks: []
   },
   mutations: {
+    loadTasks(state, payload) {
+      state.tasks = payload.tasks;
+    }
   },
   actions: {
+    async getAllTasks() {
+      const tasks = await api.getAllTasks();
+
+      commit('loadTasks', { tasks });
+    }
   },
   modules: {
   }
