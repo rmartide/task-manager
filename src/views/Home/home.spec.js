d63d07478d9f3c233395ef22ebf1a8a631287397
Rodrigo Martín de Lamo <mcgod69esp@gmail.com>
2020-04-25 21:59:28 +0200

    Added spec for home view

---
+++ src/views/Home/home.spec.js
@@ -0,0 +1,37 @@
+import { render, configure } from "@testing-library/vue";
+import { Home } from "@/views";
+import $store from "@/store/index";
+import $router from "@/router/index";
+
+jest.mock("@/store/index", () => {
+    return {
+        state: {
+            tasks: []
+        },
+        dispatch: jest.fn(() => {})
+    }
+});
+jest.mock("@/router/index");
+
+import "@testing-library/jest-dom";
+
+configure({ testIdAttribute: "data-spec" });
+
+describe("Task.vue", () => {    
+	it("component renders", () => {
+		const { getByTestId } = render(Home, {
+            mocks: {
+                $router,
+                $store
+            }
+        });
+
+		getByTestId("pageTitle");
+
+		getByTestId("taskList");
+
+        getByTestId("addTaskButton");
+        
+        expect($store.dispatch).toBeCalledWith('getUncompletedTasks');
+	});
+});
