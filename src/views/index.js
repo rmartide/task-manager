4c1beb55e9f680605eb56d503f70a20e5f56af28
Rodrigo Martín de Lamo <mcgod69esp@gmail.com>
2020-04-28 21:12:11 +0200

    Added component to index

--- src/views/index.js
+++ src/views/index.js
@@ -1,5 +1,6 @@
 import About from './About/About.vue';
 import Create from './Create/Create.vue';
 import Home from './Home/Home.vue';
+import Details from './Details/Details.vue';
 
-export { About, Create, Home };
+export { About, Create, Home, Details };
