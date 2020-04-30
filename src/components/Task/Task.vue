81a731aa497eb2d25e07696e81c356abfc766e3e
Rodrigo Martín de Lamo <mcgod69esp@gmail.com>
2020-05-01 01:33:07 +0200

    Added card style to the tasks

--- src/components/Task/Task.vue
+++ src/components/Task/Task.vue
@@ -1,11 +1,19 @@
 <template>
 	<div>
-		<h3 data-spec="taskName">{{ name }}</h3>
-		<template v-if="!showTime">
-			<span data-spec="taskDescription">{{ description }}</span>
-			<span data-spec="taskStreak">Streak: {{streak}} days</span>
-			<button data-spec="completeTaskButton" @click="handleClick">Complete</button>
-		</template>
+		<md-card md-with-hover class="md-accent">
+			<md-ripple>
+				<md-card-header>
+					<div class="md-title" data-spec="taskName">{{ name }}</div>
+				</md-card-header>
+				<md-card-content class="md-card-content">
+					<template v-if="!showTime">
+						<span data-spec="taskDescription">{{ description }}</span>
+						<span data-spec="taskStreak">Streak: {{streak}} days</span>
+						<button data-spec="completeTaskButton" @click="handleClick">Complete</button>
+					</template>
+				</md-card-content>
+			</md-ripple>
+		</md-card>
 	</div>
 </template>
 
@@ -31,3 +39,12 @@ export default {
 	}
 };
 </script>
+
+<style lang="scss" scoped>
+.md-card {
+	width: 320px;
+	margin: 4px;
+	display: inline-block;
+	vertical-align: top;
+}
+</style>
--- src/services/material.js
+++ src/services/material.js
@@ -1,8 +1,8 @@
 import Vue from "vue";
-import { MdToolbar, MdButton, MdIcon } from "vue-material/dist/components";
+import { MdToolbar, MdButton, MdIcon, MdCard, MdRipple } from "vue-material/dist/components";
 import "vue-material/dist/vue-material.min.css";
 import "vue-material/dist/theme/default.css";
 
-const components = [MdToolbar, MdButton, MdIcon];
+const components = [MdToolbar, MdButton, MdIcon, MdCard, MdRipple];
 
 components.forEach((component) => Vue.use(component));
