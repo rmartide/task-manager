1ed96c5423803838a8e48ca06215e02b0998bf61
Rodrigo Martín de Lamo <mcgod69esp@gmail.com>
2020-05-05 21:57:24 +0200

    Added some form of form validation

--- src/components/TaskForm/TaskForm.vue
+++ src/components/TaskForm/TaskForm.vue
@@ -1,22 +1,25 @@
 <template>
 	<div>
-		<div class="md-layout md-alignment-center">
-			<md-card class="md-layout-item md-small-size-50 md-medium-size-33 md-size-25">
-				<md-card-content>
-					<md-field>
-						<label>Task name</label>
-						<md-input data-spec="taskNameInput" v-model="name"></md-input>
-					</md-field>
-					<md-field>
-						<label>Task description</label>
-						<md-input data-spec="taskDescriptionInput" v-model="description"></md-input>
-					</md-field>
-				</md-card-content>
-				<md-card-actions>
-					<md-button type="submit" class="md-primary" data-spec="createTaskButton" @click="createTask">Create task</md-button>
-				</md-card-actions>
-			</md-card>
-		</div>
+		<form id="app" @submit="checkForm" action="https://vuejs.org/" method="post">
+			<div class="md-layout md-alignment-center">
+				<md-card class="md-layout-item md-small-size-50 md-medium-size-33 md-size-25">
+					<md-card-content>
+						<md-field :class="messageClass">
+							<label>Task name</label>
+							<md-input data-spec="taskNameInput" v-model="name"></md-input>
+							<span class="md-error">The name is required</span>
+						</md-field>
+						<md-field>
+							<label>Task description</label>
+							<md-input data-spec="taskDescriptionInput" v-model="description"></md-input>
+						</md-field>
+					</md-card-content>
+					<md-card-actions>
+						<md-button type="submit" class="md-primary" data-spec="createTaskButton">Create task</md-button>
+					</md-card-actions>
+				</md-card>
+			</div>
+		</form>
 	</div>
 </template>
 
@@ -29,7 +32,8 @@ export default {
 	data() {
 		return {
 			name: "",
-			description: ""
+			description: "",
+			hasMessages: false
 		};
 	},
 	methods: {
@@ -37,6 +41,28 @@ export default {
 			this.$store.dispatch('createTask', { name: this.name, description: this.description })
 
 			this.$router.push("/");
+		},
+		checkForm: function (e) {
+			if (this.name && this.age) {
+				return true;
+			}
+
+			this.errors = [];
+
+			if (!this.name) {
+				this.hasMessages = true;
+			}
+			else {
+				this.createTask();
+			}
+			e.preventDefault();
+		}
+	},
+	computed: {
+		messageClass() {
+			return {
+				'md-invalid': this.hasMessages
+			}
 		}
 	}
 };
