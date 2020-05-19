b170ef056a1e8e4310ba63f875c9dbbae89d71b9
Rodrigo Martín de Lamo <mcgod69esp@gmail.com>
2020-05-19 23:10:10 +0200

    Now the color of the speeddial's button changes if the theme is dark

--- src/ui-components/SpeedDial/SpeedDialContentButton/SpeedDialContentButton.vue
+++ src/ui-components/SpeedDial/SpeedDialContentButton/SpeedDialContentButton.vue
@@ -13,18 +13,28 @@ export default {
 	},
 	computed: {
 		active() {
-			return this.$material.theming.theme  === this.theme ? 'active' : '';
+			return this.$material.theming.theme === this.theme ? 'active' : '';
 		}
 	}
 };
 </script>
 
 <style lang="scss" scoped>
+/* @import "~vue-material/dist/theme/engine"; // Import the theme engine
+
+//md-get-color-by-type(background, dark) */
+
 .md-toolbar .md-icon-button .md-icon.active {
 	color: black !important;
+	&.md-theme-default-dark {
+		color: white !important;
+	}
 }
 
 .md-toolbar .md-icon-button .md-icon {
 	color: rgba(0, 0, 0, 0.4) !important;
+	&.md-theme-default-dark {
+		color: rgba(255, 255, 255, 0.4) !important;
+	}
 }
 </style>
