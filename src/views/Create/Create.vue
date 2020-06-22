1e4f2e41436e49f52f8044b1ec9446677ce8844e
Rodrigo Martín de Lamo <mcgod69esp@gmail.com>
2020-06-22 17:15:36 +0200

    Added test to create and logic to check if it loads the correct task if an id comes as a prop

--- src/views/Create/Create.vue
+++ src/views/Create/Create.vue
@@ -1,15 +1,27 @@
 <template>
 	<div class="create">
 		<h1 data-spec="pageTitle">New task</h1>
-		<TaskForm data-spec="taskForm"></TaskForm>
+		<TaskForm data-spec="taskForm" :task="task"></TaskForm>
 	</div>
 </template>
 
 <script>
+import api from '@/services/api';
 import {TaskForm} from "@/components/";
 
 export default {
 	name: "Create",
-	components: { TaskForm }
+	props: ['id'],
+	data() {
+		return {
+			task: null
+		}
+	},
+	components: { TaskForm },
+	mounted: async function () {
+		if (this.$props?.id !== undefined) {
+			this.task = await api.getTask(this.$props.id);
+		}
+	}
 };
 </script>
