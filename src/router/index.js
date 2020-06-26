96d32b9309f9357420f6344ea7372b2ed2e6ece1
Rodrigo Martín de Lamo <mcgod69esp@gmail.com>
2020-06-26 21:38:32 +0200

    Redirecting to home if the route doesn't exist

--- src/router/index.js
+++ src/router/index.js
@@ -6,6 +6,11 @@ Vue.use(VueRouter);
 
 const routes = [
 	{
+		path: '*',
+		name: "Any",
+		component: Home
+	},
+	{
 		path: "/",
 		name: "Home",
 		component: Home
--- src/services/driver.js
+++ src/services/driver.js
@@ -2,8 +2,6 @@ import mockdb from './mockdb';
 
 // It needs a true db driver for this to make sense
 
-console.log(process.env);
-
 const driver = process.env.VUE_APP_DRIVER === 'mock' ? mockdb : mockdb;
 
 export default driver;
